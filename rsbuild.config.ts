a69a4a14a9c5b69a90829e767f6bc6154e67cc8e
Gustavo Chagas <gustavochagasb@gmail.com>
2024-10-24 19:27:42 -0300

    Fix output asset path

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://gustavochagas.github.io/' : '/',
+  }
 });
